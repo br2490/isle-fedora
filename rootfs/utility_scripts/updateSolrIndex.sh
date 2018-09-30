46f4e6bbfc9a838aa60939eb1e01b3b6999d72d2
Benjamin Ronser <brosner@barnard.edu>
2018-09-30 19:14:23 -0400

    Final update for 1.1 release.
    Foremost this puts the Dockerfile in a logical order for build and rebuild with cache used as much as possible.
    
    Update the following and their issues:
    
    Multi-thread FGS (https://github.com/Islandora-Collaboration-Group/ISLE/issues/176) - FGS now uses 8 threads. FWIW.
    Create a helper-script to REINDEX via FGS (run `updateSolrIndex`) (https://github.com/Islandora-Collaboration-Group/ISLE/issues/173)
    Due to the long time it takes for S6 to chown/chmod large folders, deprecate it on /usr/local/fedora (https://github.com/Islandora-Collaboration-Group/ISLE/issues/171)

--- rootfs/utility_scripts/updateSolrIndex.sh
+++ rootfs/utility_scripts/updateSolrIndex.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "Okay! Reindexing your Fedora repository. This may take a while..."
-fgsUserName=$FEDORA_GSEARCH_USER fgsPassword=$FEDORA_GSEARCH_PASS .$CATALINA_HOME/webapps/fedoragsearch/client/runRESTClient.sh http://localhost:8080 updateIndex fromFoxmlFiles
+cd $CATALINA_HOME/webapps/fedoragsearch/client
+fgsUserName=$FEDORA_GSEARCH_USER fgsPassword=$FEDORA_GSEARCH_PASS ./runRESTClient.sh http://localhost:8080 updateIndex fromFoxmlFiles
