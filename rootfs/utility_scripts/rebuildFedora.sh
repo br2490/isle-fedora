e4c19e052c2f5b3774fbb28b1e6ef5a7168cab8f
Benjamin Ronser <brosner@barnard.edu>
2018-10-09 09:27:42 -0400

    Simplify the script and add some time to read.

--- rootfs/utility_scripts/rebuildFedora.sh
+++ rootfs/utility_scripts/rebuildFedora.sh
@@ -2,25 +2,26 @@
 
 FEDORA_RUNNING=`curl --user $TOMCAT_ADMIN_USER:$TOMCAT_ADMIN_PASS http://localhost:8080/manager/text/list | grep fedora:running | wc -l`
 if [ ${FEDORA_RUNNING} -eq "1" ]; then
-	echo "Stopping FEDORA."
-	curl --user $TOMCAT_ADMIN_USER:$TOMCAT_ADMIN_PASS http://localhost:8080/manager/text/stop?path=/fedora
+        echo "Stopping FEDORA."
+        curl --user $TOMCAT_ADMIN_USER:$TOMCAT_ADMIN_PASS http://localhost:8080/manager/text/stop?path=/fedora
 fi
-
-echo 'Truncating old SQL tables.'
-mysql -hmysql -u$FEDORA_DB_USER -p$FEDORA_DB_PASS $FEDORA_DB --execute 'TRUNCATE TABLE dcDates; TRUNCATE TABLE doFields; TRUNCATE TABLE doRegistry; TRUNCATE TABLE pidGen;'
 
 echo 'Starting the rebuild process in the background. This may take a while depending on your Fedora repository size.'
 echo 'To watch the log and process run: tail -f $CATALINA_HOME/logs/fedora-rebuild.out'
 
 cd $FEDORA_HOME/server/bin || exit;
-nohup \
-	fedora-rebuild.sh -r org.fcrepo.server.resourceIndex.ResourceIndexRebuilder >> $CATALINA_HOME/logs/fedora-rebuild.out 2>> $CATALINA_HOME/logs/fedora-rebuild.err && \
-	fedora-rebuild.sh -r org.fcrepo.server.utilities.rebuild.SQLRebuilder >> $CATALINA_HOME/logs/fedora-rebuild.out 2>> $CATALINA_HOME/logs/fedora-rebuild.err && \
-	curl --user $TOMCAT_ADMIN_USER:$TOMCAT_ADMIN_PASS http://localhost:8080/manager/text/start?path=/fedora < /dev/null &
+echo 'Truncating old SQL tables.'
+mysql -hmysql -u$FEDORA_DB_USER -p$FEDORA_DB_PASS $FEDORA_DB --execute 'TRUNCATE TABLE dcDates; TRUNCATE TABLE doFields; TRUNCATE TABLE doRegistry; TRUNCATE TABLE pidGen;'
+
+sleep 10; # Let the user read.
+
+nohup bash -c 'fedora-rebuild.sh -r org.fcrepo.server.resourceIndex.ResourceIndexRebuilder && \
+fedora-rebuild.sh -r org.fcrepo.server.utilities.rebuild.SQLRebuilder && \
+curl --user $TOMCAT_ADMIN_USER:$TOMCAT_ADMIN_PASS http://localhost:8080/manager/text/start?path=/fedora' >> $CATALINA_HOME/logs/fedora-rebuild.out 2>> $CATALINA_HOME/logs/fedora-rebuild.err < /dev/null &
 
 echo 'Automatically tailing the log file...'
 echo 'Press CTRL+C to stop watching at any time. This will NOT stop the rebuild process.'
-sleep 3;
+sleep 10; # Let the user read.
 tail -f $CATALINA_HOME/logs/fedora-rebuild.out
 
 echo 'To watch the log and process run: tail -f $CATALINA_HOME/logs/fedora-rebuild.out'
